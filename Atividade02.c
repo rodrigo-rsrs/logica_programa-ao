983db529b71feb4df27e06cc59ed13c74b6660f1
Rodrigo Jesus Sena dos Santos <rjssantos.2007@gmail.com>
2026-05-28 09:50:36 -0300

    Atualizar o Atividade-02.c

--- Atividade02.c
+++ Atividade02.c
@@ -18,7 +18,7 @@ int main() {
         
     }else if(imc >= 18.5 && imc < 25){
         printf("Peso normal");
-        
+         
     }else if(imc >= 25 && imc < 30){
         printf("Acima do peso");
         
