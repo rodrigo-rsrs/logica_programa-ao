782b2bc0fa292d75b5c63be4c13d6817e4b0880d
Rodrigo Jesus Sena dos Santos <rjssantos.2007@gmail.com>
2026-05-28 09:36:34 -0300

    Atualizar o Atividade-02.c

--- Atividade02.c
+++ Atividade02.c
@@ -1,36 +1,30 @@
 #include <stdio.h>
 
 int main() {
-
+    
     float peso;
     float altura;
-    float imc;
-
-    printf("Digite seu peso: ");
+    
+    printf("Informe seu peso: ");
     scanf("%f", &peso);
-
-    printf("Digite sua altura: ");
+    
+    printf("Informe sua altura: ");
     scanf("%f", &altura);
-
-    imc = peso / (altura * altura);
-
-    printf("Seu IMC e: %.2f\n", imc);
-
-    if (imc < 18.5) {
+    
+      float imc = peso / (altura * altura);
+    
+    if(imc < 18.5){
         printf("Abaixo do peso");
-    }
-
-    if (imc >= 18.5 && imc < 25) {
-        printf("Normal");
-    }
-
-    if (imc >= 25 && imc < 30) {
-        printf("Sobrepeso");
-    }
-
-    if (imc >= 30) {
+        
+    }else if(imc >= 18.5 && imc < 25){
+        printf("Peso normal");
+        
+    }else if(imc >= 25 && imc < 30){
+        printf("Acima do peso");
+        
+    }else{
         printf("Obesidade");
     }
-
+    
     return 0;
 }
